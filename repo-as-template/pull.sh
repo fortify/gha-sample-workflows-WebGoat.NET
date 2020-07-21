b2b41c44ce9f921015906706a9f826911682ed78
rsenden <ruud.senden@microfocus.com>
2020-07-21 15:09:14 +0200

    Setup as derived repository

---
+++ repo-as-template/pull.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script pulls the latest changes from the main sample repository
+git pull "https://github.com/jerryhoff/WebGoat.NET.git" --allow-unrelated-histories --no-edit
---
+++ run-after-clone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script is to be run after cloning the repo into a new directory
+# This will configure the repository with the 'ignore' merge driver
+git config merge.ignore.driver true
+
